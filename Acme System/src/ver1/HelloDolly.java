34498796fceb7d8f6907aba3a22eb4390c83c47f
Hoyong Lee <yongmon01@gmail.com>
2019-09-05 03:58:48 +0900

    Hello Dolly added from Eclips

--- Acme System/src/ver1/HelloDolly.java
+++ Acme System/src/ver1/HelloDolly.java
@@ -5,6 +5,7 @@ public class HelloDolly {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("print");
+		System.out.println("\nHello Dolly from Eclipse Again");
 	}
 
 }
